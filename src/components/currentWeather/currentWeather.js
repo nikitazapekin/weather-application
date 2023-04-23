a50c61f00da918873ae8bf653eed8f467631baa9
Nikita Zapekin <wotblitz362@mail.ru>
2023-04-23 20:36:20 +0300

    adding current temperature

--- src/components/currentWeather/currentWeather.js
+++ src/components/currentWeather/currentWeather.js
@@ -1,7 +1,61 @@
-export const CurrentWeather=(props)=> {
-return (
+
+import { useState, useEffect } from "react"
+import "./currentWeather.css"
+export const CurrentWeather = (props) => {
+  const { city, newArr, temp, date } = props
+  const [arrayOfMin, setArrayOfMin] = useState([])
+const [arrayOfMax, setArrayOfMax]=useState([])
+const [datas, setDatas]=useState([])
+  useEffect(() => {
+    const tempArray = []
+    const maxTempArray=[]
+    const arrayOfDatas=[]
+    for (let i = 0; i < temp.length; i+=24) {
+        const currentData=date[i].slice(0,10)
+        arrayOfDatas.push(currentData)
+      let currentMin = 100
+
+      for (let j = i; j < i+24 && j < temp.length; j++) {
+        if (temp[j] < currentMin) {
+          currentMin = temp[j]
+        }
+      }
+      tempArray.push(Math.round(currentMin))
+    }
+    setArrayOfMin(tempArray)
+setDatas(arrayOfDatas)
+
+    for (let i = 0; i < temp.length; i+=24) {
+        let currentMax = -100
+        for (let j = i; j < i+24 && j < temp.length; j++) {
+          if (temp[j] > currentMax) {
+            currentMax = temp[j]
+          }
+        }
+        maxTempArray.push(Math.round(currentMax))
+      }
+
+setArrayOfMax(maxTempArray)
+
+  }, [temp])
+
+
+
+  const renderMin = arrayOfMin.map(item => {
+    return <div>{item}</div>
+  })
+  const renderMax = arrayOfMax.map(item => {
+    return <div>{item}</div>
+  })
+  const renderItems=renderMax.map((item, index)=> {
+    return <div className="renderedCurrentCityTemperature">{arrayOfMin[index]}-{item }<br /> {datas[index]} <div className="renderedCurrentCityTemperatureFon"></div></div>
+  })
+  //console.log(datas)
+  return (
     <div className="currentWeather">
-        ffff
+      <div className="todayIn">Today in {city} </div>
+     <div className="tableTemperature">{renderItems}</div>
+     
     </div>
-)
+  )
 }
--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -72,13 +72,20 @@ const style={
 display:  boolCheck ? "none" : "block"
 }
 console.log("bool" +boolCheck) */
+
+
+
+
+
+/*
 import { useState, useEffect } from 'react';
 import "./homepage.css"
 import { InputSearch } from '../inputSearch/inputSearch';
 import MyComponent from '../test/test';
 import { Navigation } from '../date/navigation/navigation';
 import { CurrentWeather } from '../currentWeather/currentWeather';
-
+//import translate from 'translate-google';
+import translate from 'translate-google';
 const Homepage = () => {
 const [city, setCity ]=useState("")
 const [newArr, setNewArr]=useState([])
@@ -134,6 +141,96 @@ console.log("Geolocation не поддерживается вашим брауз
 setBoolCheck(false) // hide the loading image
 }
 }, []);
+ */
+
+
+
+
+
+
+
+
+
+
+import React, { useState, useEffect } from 'react';
+import "./homepage.css";
+import { InputSearch } from '../inputSearch/inputSearch';
+import MyComponent from '../test/test';
+import { Navigation } from '../date/navigation/navigation';
+import { CurrentWeather } from '../currentWeather/currentWeather';
+//import translate from 'translate-google';
+
+const Homepage = () => {
+  const [city, setCity] = useState("");
+  const [newArr, setNewArr] = useState([]);
+  const [temp, setTemp]=useState([])
+  const [date, setDate]=useState([])
+  const [boolCheck, setBoolCheck] = useState(true); // initialize with true
+
+  useEffect(() => {
+    function successCallback(position) {
+      setBoolCheck(false); // hide the loading image
+      const geoUrl = `https://api.opencagedata.com/geocode/v1/json?q=${position.coords.latitude}+${position.coords.longitude}&key=f9629a9e6fd7493aac20c35043c7e411`;
+      fetch(geoUrl)
+        .then(responses => responses.json())
+        .then(data => {
+          const city = data.results[0].components.city;
+         // console.log(city)
+          setCity(city)
+       /*   const encodedCity = encodeURIComponent(city);
+          const translateUrl = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${encodedCity}`;
+          fetch(translateUrl)
+            .then(response => response.json())
+            .then(data => {
+           //   const translation = data[0][0][0];
+             // console.log(`Your city: ${translation}`);
+            //  setCity(translation);
+            }); */
+        });
+      
+      async function fetchAsyncTodos() {
+        const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&hourly=temperature_2m&forecast_days=16`;
+        const response = await fetch(weatherUrl);
+        const data = await response.json();
+        const arrayOfTemp = data.hourly.temperature_2m.map(item => item);
+        const arrayOfTime = data.hourly.time.map(item => item);
+        const newArr = arrayOfTemp.concat(arrayOfTime);
+        setTemp(arrayOfTemp)
+        setDate(arrayOfTime)
+        setNewArr(newArr);
+      }
+      fetchAsyncTodos();
+    }
+
+    function errorCallback(error) {
+      let errorMessage = "";
+      switch(error.code) {
+        case error.PERMISSION_DENIED:
+          errorMessage = "Пользователь отказался предоставить местоположение";
+          break;
+        case error.POSITION_UNAVAILABLE:
+          errorMessage = "Местоположение недоступно";
+          break;
+        case error.TIMEOUT:
+          errorMessage = "Таймаут при запросе местоположения";
+          break;
+        default:
+          errorMessage = "Произошла неизвестная ошибка";
+      }
+      console.log(errorMessage);
+      setBoolCheck(false); // hide the loading image
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    } else {
+      console.log("Geolocation не поддерживается вашим браузером");
+      setBoolCheck(false); // hide the loading image
+    }
+  }, []);
+
+
+
 
 const len =newArr.length/2;
 const finalRender=newArr.slice(0, len).map((item, index)=> {
@@ -143,6 +240,7 @@ const style={
 display: boolCheck ? "block" : "none" // show the loading image if boolCheck is true
 }
 console.log("bool" +boolCheck)
+console.log(city)
   return (
     <div className="homepage">
        <img style={style} className='loadBar' src="https://media.giphy.com/media/17mNCcKU1mJlrbXodo/giphy.gif" alt="loading" />
@@ -150,7 +248,7 @@ console.log("bool" +boolCheck)
       <Time />
       <img className='backgroundFon' src="https://media.giphy.com/media/xT0xeNf2csFIbeAxvq/giphy.gif" alt="fon" />
    {/*   {finalRender} */}
-      <CurrentWeather newArr={newArr} city={city}/>
+      <CurrentWeather temp={temp} date={date} newArr={newArr} city={city}  />
      
     </div>
   )
